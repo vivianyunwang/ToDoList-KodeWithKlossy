807ea91a9a352e38a8060a13611d269b6b65f261
vivianyunwang <vivianyunwang@gmail.com>
2020-07-14 13:26:13 -0700

    Testing out how to use commit!

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,15 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Vivian Wang on 7/14/20.
+//  Copyright © 2020 Vivian Wang. All rights reserved.
+//
+
+import UIKit
+
+class ToDo
+{
+    var name = "";
+    var important = false
+}
